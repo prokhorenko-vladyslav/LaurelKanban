dfb9668ac1792da3e9a1f5830166138aba348bbf
Prokhorenko Vladyslav <random.person.1907@gmail.com>
2020-04-18 17:42:55 +0300

    Added KanbanProvider

---
+++ src/Providers/KanbanProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Laurel\Kanban\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class KanbanProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
