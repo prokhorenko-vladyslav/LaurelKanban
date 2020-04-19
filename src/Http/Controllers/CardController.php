8a64600fb1c5fb91220f2e219911f6e060e3a8df
Prokhorenko Vladyslav <random.person.1907@gmail.com>
2020-04-19 15:43:52 +0300

    Added show method for the CardController.

--- src/Http/Controllers/CardController.php
+++ src/Http/Controllers/CardController.php
@@ -4,6 +4,7 @@ namespace Laurel\Kanban\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Laurel\Kanban\Http\Requests\CardShow;
 use Laurel\Kanban\Http\Requests\CardIndex;
 use Laurel\Kanban\Http\Resources\CardResource;
 use Laurel\Kanban\Http\Resources\CardShortResource;
@@ -19,5 +20,16 @@ class CardController extends Controller
             $cards = [];
         }
         return CardShortResource::collection($cards);
+    }
+
+    public function show(CardShow $request, int $deskId, int $cardId)
+    {
+        try {
+            return new CardResource(
+                Desk::findOrFail($deskId)->cards()->findOrFail($cardId)
+            );
+        } catch (\Exception $e) {
+            return response('', 404);
+        }
     }
 }
